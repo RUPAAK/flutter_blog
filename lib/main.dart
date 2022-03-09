5679f035e9ab632f043b20152a2ca2eddd8577ea
rupaak <rupakt525@gmail.com>
2022-03-09 22:17:11 +0545

    inital routing fixed

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
           child: Scaffold(
               body: initScreen == 0 || initScreen == null
                   ? FirstScreen()
-                  : OptionScreen()
+                  : LoginEmail()
                   )),
     );
   }
