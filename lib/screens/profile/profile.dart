aab9655e815a7fb1f3578a489fc6081ae439e4e7
rupaak <rupakt525@gmail.com>
2022-03-12 20:54:03 +0545

    user creadiential screen complete

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,15 +1,13 @@
+import 'package:blog_app/common/animation/slide-transistion-route.dart';
 import 'package:blog_app/homepage.dart';
-import 'package:blog_app/screens/subcription/subscriptions.dart';
+import 'package:blog_app/screens/login/login-options.dart';
+import 'package:blog_app/screens/login/login-password.dart';
+import 'package:blog_app/screens/login/reset-password.dart';
 import 'package:flutter/material.dart';
 
-class Profile extends StatefulWidget {
+class Profile extends StatelessWidget {
   const Profile({Key? key}) : super(key: key);
 
-  @override
-  State<Profile> createState() => _ProfileState();
-}
-
-class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     double deviceHeight(BuildContext context) =>
@@ -17,104 +15,113 @@ class _ProfileState extends State<Profile> {
 
     return SafeArea(
         child: Scaffold(
-      body: Container(
-          child: Column(
+            body: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0, bottom: 50.0),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => ResetPassword()),
+                    (route) => false);
+              },
+              child: Icon(Icons.arrow_back, color: Colors.white),
+              style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                primary: Colors.blue, // <-- Button color
+                onPrimary: Colors.red, // <-- Splash color
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 18, right: 18),
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 20.0, bottom: 50),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                        onPressed: () {},
-                        child: Icon(
-                          Icons.arrow_back_sharp,
-                          color: Colors.white,
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          shape: CircleBorder(),
-                          primary: Colors.blue, // <-- Button color
-                          onPrimary: Colors.red,
-                        ))
-                  ]),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 18, right: 18),
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.8,
-                // color: Colors.red,
-                child: Text(
-                  "Start Your Profile",
-                  style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 0.4),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20.0, bottom: 50),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 18, right: 18),
-                child: Container(
+                Container(
                   width: MediaQuery.of(context).size.width * 0.8,
                   // color: Colors.red,
                   child: Text(
-                    "This is how you're displayed in community",
+                    "Start Your Profile",
                     style: TextStyle(
-                        fontSize: 15,
+                        fontSize: 25,
                         fontWeight: FontWeight.bold,
                         letterSpacing: 0.4),
                   ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20.0, bottom: 50),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 18, right: 18),
-                child: Container(
-                  width: 90,
-                  height: 90,
-                  // color: Colors.red,
-                  decoration: BoxDecoration(
-                      border: Border.all(width: 4, color: Colors.white),
-                      boxShadow: [
-                        BoxShadow(
-                          spreadRadius: 2,
-                          blurRadius: 10,
-                          color: Colors.black.withOpacity(0.1),
-                        )
-                      ],
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: NetworkImage(
-                              'https://www.filmibeat.com/ph-big/2011/09/1316088442375379.jpg'))),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    // color: Colors.red,
+                    child: Text(
+                      "This is how you're displayed in community.",
+                      style: TextStyle(fontSize: 15, letterSpacing: 0.4),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 80.0),
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                child: FloatingActionButton.extended(
-                  onPressed: () {
-                    Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(builder: (context) => Home()),
-                        (route) => false);
-                  },
-                  heroTag: null,
-                  label: Text('Continue'), // <-- Text
-                  backgroundColor: Colors.red,
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        radius: 30.0,
+                        backgroundImage: NetworkImage(
+                            'https://imgs.search.brave.com/GebT8qoi4y9AtgQ9-87vklSINNzkH08a5gzgr5X0Ai8/rs:fit:980:980:1/g:ce/aHR0cHM6Ly9jZG4u/b25saW5ld2ViZm9u/dHMuY29tL3N2Zy9p/bWdfODcyMzcucG5n'),
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.6,
+                              child: TextFormField(
+                                decoration: const InputDecoration(
+                                  border: UnderlineInputBorder(),
+                                  labelText: 'Username or Email',
+                                ),
+                              ),
+                            ),
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.6,
+                              child: TextFormField(
+                                decoration: const InputDecoration(
+                                  border: UnderlineInputBorder(),
+                                  labelText: 'Username or Email',
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 80.0),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: FloatingActionButton.extended(
+                      onPressed: () {
+                        Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(builder: (context) => Home()),
+                            (route) => false);
+                      },
+                      heroTag: null,
+                      label: Text('Continue'), // <-- Text
+                      backgroundColor: Colors.red,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ])),
-    ));
+          )
+        ])));
   }
 }
