46f262ac1a1607f2063a111feca6bf19ffc2f22e
ShresthRojesh <rojeshshrestha135@hotmail.com>
2022-03-19 13:35:13 +0545

    comment

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -356,6 +356,7 @@ class _HomeState extends State<Home> {
                                     ],
                                   ),
                                 ),
+                                
                                 Padding(
                                   padding: const EdgeInsets.symmetric(
                                       vertical: 10.0),
--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,4 +1,5 @@
 import 'package:blog_app/common/animation/slide-transistion-route.dart';
+import 'package:blog_app/screens/comment.dart';
 import 'package:blog_app/screens/home.dart';
 import 'package:blog_app/screens/login/login-options.dart';
 import 'package:blog_app/screens/login/login-password.dart';
@@ -113,7 +114,7 @@ class Profile extends StatelessWidget {
                       onPressed: () {
                         Navigator.pushAndRemoveUntil(
                             context,
-                            MaterialPageRoute(builder: (context) => MyProfile()),
+                            MaterialPageRoute(builder: (context) => Comment()),
                             (route) => false);
                       },
                       heroTag: null,
